1bf82f6f9adb2efaf1e284b96d2aa3a817dc440f
Samuel Alves <alvessamuel12@outlook.com>
2022-04-08 12:54:24 -0300

    Continuação do POST da compra

--- src/main/java/br/com/letscode/dataproject/purchase/service/PurchaseService.java
+++ src/main/java/br/com/letscode/dataproject/purchase/service/PurchaseService.java
@@ -6,6 +6,7 @@ import br.com.letscode.dataproject.customer.service.CustomerService;
 import br.com.letscode.dataproject.product.model.Product;
 import br.com.letscode.dataproject.product.service.ProductService;
 import br.com.letscode.dataproject.purchase.dto.PurchaseDTO;
+import br.com.letscode.dataproject.purchase.dto.PurchaseProductDTO;
 import br.com.letscode.dataproject.purchase.model.Purchase;
 import br.com.letscode.dataproject.purchase.repository.PurchaseRepository;
 import br.com.letscode.dataproject.purchaseproduct.model.PurchaseProduct;
@@ -16,6 +17,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -40,22 +42,25 @@ public class PurchaseService {
         return purchaseRepository.findByCustomer(customer, pageable).map(PurchaseDTO::convert);
     }
 
-    public PurchaseDTO createPurchase(PurchaseDTO purchase) {
+    public PurchaseDTO createPurchase(PurchaseDTO purchase) throws Exception {
         Customer customer = customerService.getCustomerByRegistration(purchase.getCustomerPurchase().getRegistrationNumber());
+
+        List<Product> productsSold = purchase.getProductsSold().stream().map(pProductDto -> productService.findByCodeNumber(pProductDto.getProduct().getCodeNumber())).collect(Collectors.toList());
+        BigDecimal purchaseValueCalculated = calculateValueOfPurchase(productsSold);
+        BigDecimal purchaseRequestValue = calculateValueOfPurchaseRequest(purchase.getProductsSold());
+        if(purchaseRequestValue.doubleValue() != purchaseValueCalculated.doubleValue()) {
+            throw new Exception("The total value of purchase dont't close with products value. Please contact the support!");
+        }
         Purchase p = new Purchase(
             purchase.getPurchaseDate(),
-            purchase.getValue(),
+            purchaseRequestValue,
             customer
         );
 
         p = purchaseRepository.save(p);
 
-        // erro desconhecido 
-        // List<Product> productsSold = purchase.getProductsSold().stream().map(p -> {
-        //     return productService.findByCodeNumber(p.getProduct().getCodeNumber());
-        // }).collect(Collectors.toList());
-
-        // p.getPurchaseProductList().addAll(productsSold);
+        // adicionando os produtos no list de produtos da compra para salvar no final
+        p.getPurchaseProductList().addAll(productsSold);
         
 
         // TODO precisa-se saber o momento de criar o purchase_product para poder iterar por cada produto, pegar seu id e código especifico e salvar na tupla
@@ -65,5 +70,21 @@ public class PurchaseService {
         // }).collect(Collectors.toList());
 
         return null;
+    }
+
+    private static BigDecimal calculateValueOfPurchaseRequest(List<PurchaseProductDTO> products) {
+        Double sum = 0D;
+        for(PurchaseProductDTO product : products) {
+            sum += product.getProduct().getPrice().doubleValue();
+        }
+        return BigDecimal.valueOf(sum);
+    }
+
+    private static BigDecimal calculateValueOfPurchase(List<Product> products) {
+        Double sum = 0D;
+        for(Product product : products) {
+            sum += product.getPrice().doubleValue();
+        }
+        return BigDecimal.valueOf(sum);
     }
 }
